8c3bf31d569ab36590052ec4cbc37ac7f056726e
Bharat Gidwani <bharat.gidwani@gmail.com>
2020-04-03 20:57:28 -0400

    Display updated at for each article

--- src/app/articles-topheadlines/articles-topheadlines.component.ts
+++ src/app/articles-topheadlines/articles-topheadlines.component.ts
@@ -5,16 +5,14 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-articles-topheadlines',
   templateUrl: './articles-topheadlines.component.html',
-  styleUrls: ['./articles-topheadlines.component.scss']
+  styleUrls: ['./articles-topheadlines.component.scss'],
 })
 export class ArticlesTopheadlinesComponent implements OnInit {
   public articles: Observable<any>;
 
-  constructor(private newsApi: NewsapiService) { }
+  constructor(private newsApi: NewsapiService) {}
 
   ngOnInit(): void {
-    this.newsApi.getTopHeadlines()
-        .subscribe(data => this.articles = data);
+    this.newsApi.getTopHeadlines().subscribe((data) => (this.articles = data));
   }
-
 }
--- src/app/services/newsapi.service.ts
+++ src/app/services/newsapi.service.ts
@@ -5,26 +5,25 @@ import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NewsapiService {
   private apiKey = environment.newsApiKey;
+  private baseUrl = 'https://newsapi.org/v2';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getTopHeadlines(): Observable<any> {
-    const url = 'https://newsapi.org/v2/top-headlines?language=en&country=us&apiKey=' + this.apiKey;
+    const url = `${this.baseUrl}/top-headlines?language=en&country=us&apiKey=${this.apiKey}`;
     return this.getNewsData(url);
   }
 
   getArticlesTechnology(): Observable<any> {
-    const url = 'https://newsapi.org/v2/top-headlines?category=technology&language=en&country=us&apiKey=' + this.apiKey;
+    const url = `${this.baseUrl}/top-headlines?category=technology&language=en&country=us&apiKey=${this.apiKey}`;
     return this.getNewsData(url);
   }
 
   private getNewsData(url: string): Observable<any> {
-    return this.http
-        .get(url)
-        .pipe(map((data: any) => data.articles));
+    return this.http.get(url).pipe(map((data: any) => data.articles));
   }
 }
